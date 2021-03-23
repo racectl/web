9fe658746a6c8b2d27a9f580748034b68b010a60
Dale Carter <xthedalex@gmail.com>
2021-03-23 06:55:59 -0400

    Alertable Exception Concept

---
+++ app/Exceptions/AlertableException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+/**
+ * Parent exception for ones that should be caught and displayed to a user.
+ */
+abstract class AlertableException extends Exception
+{
+
+}
